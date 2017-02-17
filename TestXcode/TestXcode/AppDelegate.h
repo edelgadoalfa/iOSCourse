f6256480c09628ad6a986c3192d0202e7026df3b
Edgar Delgado <5587edgar@gmail.com>
2017-02-17 17:53:03 -0600

    testin git from terminal

---
+++ TestXcode/TestXcode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestXcode
+//
+//  Created by Edgar Delgado on 2/17/17.
+//  Copyright © 2017 Edgar Delgado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
